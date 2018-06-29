9627e8030650f9b5a6dc45f105c7b50e01d86d37
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2018-06-29 15:41:25 -0500

    Assure login paths are not blocked by authentication action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::UsersController < ApplicationController
+	skip_before_action :authenticate_request, only: [:login]
 
 	def index
 		@users = User.all
@@ -11,6 +12,11 @@ class Api::UsersController < ApplicationController
 	def create
 		@user = User.create!(user_params)
 		render status: :created
+	end
+
+	def login
+		@token = AuthenticateUser.call(user_params[:email], user_params[:password])
+		render json: { token: @token.result }
 	end
 
 	private
